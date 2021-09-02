e3579d11f39ecf23a9dcaf9e68403ac3fadb8da7
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-09-02 10:51:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import { refs } from './js/refs';
 import searchImages from './js/apiService';
 
 refs.submitBtn.addEventListener('click', searchImages)
-refs.input.addEventListener('input', cleanInput);
 
 
