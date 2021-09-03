33dba8549d7fbe267a00248b062a4cbe84470c44
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-09-03 09:46:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { refs } from './js/refs';
 import searchImages from './js/apiService';
+import { alert, error, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+
 
 refs.submitBtn.addEventListener('click', searchImages)
 
