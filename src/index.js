943cfff72e3b12066b88dfc88522b229d240065a
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-09-03 18:57:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,65 +1,66 @@
 import searchImages from './js/apiService';
 import markup from './templates/markup.hbs';
-
+import '../node_modules/@pnotify/core/BrightTheme.css';
+import { error, notice } from '../node_modules/@pnotify/core/dist/PNotify.js';
 
 const refs = {
-    input: document.querySelector('input'),
-    form: document.querySelector('#search-form'),
-    container: document.querySelector('.container'),
-}
+  input: document.querySelector('input'),
+  form: document.querySelector('#search-form'),
+  container: document.querySelector('.container'),
+  submitBtn: document.querySelector('#submit')
+};
 
 let page = 1;
 let btn = null;
 let upBtn = null;
 
 refs.form.addEventListener('submit', searchImagesOnBtnClick);
-refs.input.addEventListener('input', cleanInput)
-
+refs.input.addEventListener('input', cleanInput);
 
 function searchImagesOnBtnClick(evt) {
-      evt.preventDefault();
-    
-    const searchValue = refs.input.value;     
-       
- if (evt.target.className === ('search-form')) {
-        page = 1;
-        return searchImages(searchValue.trim(), page)
-            .then(data => createFirstListMarkup(data))
-            .catch(error => console.log(error));
-    }
+  evt.preventDefault();
 
-    else if (evt.target.className === ('loadMoreBtn button')) {
-        page += 1;
-        return searchImages(searchValue.trim(), page)
-            .then(data => createNextListMarkup(data))
-            .catch(error => console.log(error));
-    
-    } 
- else if (searchImages(searchValue.trim(), page).then(result => result.hits.length === 0))
-        {
-            refs.input.value = '';
-            return;
-}
-            }
-    
+  const searchValue = refs.input.value;
 
-function createFirstListMarkup(data) {
-    createList();
-    createListItems(data);
-    createBtn();
-    scrolling();
-
+    searchImages(searchValue.trim(), page).then(result => {
+        if (result.hits.length === 0) {
+      error({
+          title: 'OOPS :-(',
+          text: 'Wrong request! Try again!'
+      });
+    } else if (evt.target.className === 'search-form') {
+    notice({
+        title: 'Great !!!',
+        text: ` There are ${result.total} results`,
+      });
+    page = 1;
+    return searchImages(searchValue.trim(), page)
+      .then(data => createList(data))
+      .then(i => scrolling(i))
+      .catch(error => console.log(error));
+  } else if (evt.target.className === 'loadMoreBtn button') {
+    page += 1;
+    return searchImages(searchValue.trim(), page)
+      .then(data => createListItems(data))
+      .then(i => scrolling(i))
+      .catch(error => console.log(error));
+  }
+    
     return;
+  });
+
+  return;
 };
-function createNextListMarkup(data) {
-    createListItems(data);
-    scrolling();
-};
-//создает ul
-function createList() {
+
+
+//создает ul и li
+function createList(data) {
+    refs.submitBtn.disabled = ('disabled');
   const list = document.createElement('ul');
   list.classList.add('gallery');
   refs.container.insertAdjacentElement('beforeend', list);
+  createListItems(data);
+  createBtn();
   return;
 }
 //создает разметку галлереи
@@ -71,7 +72,7 @@ function createListItems(data) {
   list.insertAdjacentHTML('beforeend', markupEl);
 }
 
- //создает кнопки ""загрузить больше" и "вверх""
+//создает кнопки ""загрузить больше" и "вверх""
 function createBtn() {
   const loadMoreBtn = document.createElement('button');
   loadMoreBtn.textContent = 'Load more';
@@ -89,7 +90,7 @@ function createBtn() {
   refs.container.insertAdjacentElement('beforeend', upBtn);
 }
 
- //скролит до кнопки
+//скролит до кнопки
 function scrolling() {
   btn = document.querySelector('.loadMoreBtn');
   btn.scrollIntoView({
@@ -99,20 +100,18 @@ function scrolling() {
 }
 // очищает разметку галлереи
 function cleanInput() {
+        refs.submitBtn.disabled = false;
+
   if (refs.input.value === '') {
-      page = 1;
-      btn = document.querySelector('.loadMoreBtn')
-    btn.remove();
-    upBtn = document.querySelector('.upBtn');
-    upBtn.remove();
-    const list = document.querySelector('ul');
-    list.remove();
-    return;
+    page = 1;
+    if (document.querySelector('.gallery')) {
+      btn = document.querySelector('.loadMoreBtn');
+      btn.remove();
+      upBtn = document.querySelector('.upBtn');
+      upBtn.remove();
+      const list = document.querySelector('ul');
+      list.remove();
+      return;
+    }
   }
 }
-
-
-
-
-
-//
