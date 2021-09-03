7126d6279166c52b71965a7703db38b618357901
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-09-03 15:23:51 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,96 +1,107 @@
-import { refs } from './refs';
-import markup from '../templates/markup.hbs';
-
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '23114127-913a50287ab6c0ea340feb686';
 
-refs.input.addEventListener('input', cleanInput);
-
-let page = 1;
-let searchValue = '';
-
-export default function searchImages(evt) {
-  evt.preventDefault();
-
-  searchValue = refs.input.value;
+export default function searchImages(searchValue, page) {
   return fetch(
     `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchValue}&page=${page}&per_page=12&key=${KEY}`,
-  )
-    .then(response => response.json())
-    .then(i => createListMarkup(i))
-    .then(() => page++);
+  ).then(response => response.json()).catch(error => console.log(error));
 }
 
-let btn = null;
-let upBtn = null;
 
-function createListMarkup(data) {
-  if (page === 1) {
-    createList();
-    createListItems(data);
-    createBtn();
-    scrolling();
 
-    return;
-  }
-  createListItems(data);
-  scrolling();
+// import { refs } from './refs';
+// import markup from '../templates/markup.hbs';
 
-  return;
-}
+// const BASE_URL = 'https://pixabay.com/api/';
+// const KEY = '23114127-913a50287ab6c0ea340feb686';
 
-function cleanInput() {
-  if (refs.input.value === '') {
-    page = 1;
-    btn.remove();
-    upBtn = document.querySelector('.upBtn');
-    upBtn.remove();
-    const list = document.querySelector('ul');
-    list.remove();
-    return;
-  }
-}
+// refs.input.addEventListener('input', cleanInput);
 
-//скролит до кнопки
-function scrolling() {
-  btn = document.querySelector('.loadMoreBtn');
-  btn.scrollIntoView({
-    behavior: 'smooth',
-    block: 'end',
-  });
-}
+// let page = 1;
+// let searchValue = '';
 
-//создает ul
-function createList() {
-  const list = document.createElement('ul');
-  list.classList.add('gallery');
-  refs.container.insertAdjacentElement('beforeend', list);
-  return;
-}
+// export default function searchImages(evt) {
+//   evt.preventDefault();
 
-//создает разметку галлереи
-function createListItems(data) {
-  const { hits } = data;
-  const markupEl = markup(hits);
+//   searchValue = refs.input.value;
+//   return fetch(
+//     `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchValue}&page=${page}&per_page=12&key=${KEY}`,
+//   )
+//     .then(response => response.json())
+//     .then(i => createListMarkup(i))
+//     .then(() => page++);
+// }
 
-  const list = document.querySelector('.gallery');
-  list.insertAdjacentHTML('beforeend', markupEl);
-}
+// let btn = null;
+// let upBtn = null;
 
-//создает кнопки ""загрузить больше" и "вверх""
-function createBtn() {
-  const loadMoreBtn = document.createElement('button');
-  loadMoreBtn.textContent = 'Load more';
-  loadMoreBtn.classList.add('loadMoreBtn');
-  loadMoreBtn.classList.add('button');
+// function createListMarkup(data) {
+//   if (page === 1) {
+//     createList();
+//     createListItems(data);
+//     createBtn();
+//     scrolling();
 
-  refs.container.insertAdjacentElement('beforeend', loadMoreBtn);
-  loadMoreBtn.addEventListener('click', searchImages);
+//     return;
+//   }
+//   createListItems(data);
+//   scrolling();
 
-  const upBtn = document.createElement('a');
-  upBtn.textContent = 'Up';
-  upBtn.classList.add('upBtn');
-  upBtn.href = '#submit';
+//   return;
+// }
 
-  refs.container.insertAdjacentElement('beforeend', upBtn);
-}
+// function cleanInput() {
+//   if (refs.input.value === '') {
+//     page = 1;
+//     btn.remove();
+//     upBtn = document.querySelector('.upBtn');
+//     upBtn.remove();
+//     const list = document.querySelector('ul');
+//     list.remove();
+//     return;
+//   }
+// }
+
+// //скролит до кнопки
+// function scrolling() {
+//   btn = document.querySelector('.loadMoreBtn');
+//   btn.scrollIntoView({
+//     behavior: 'smooth',
+//     block: 'end',
+//   });
+// }
+
+// //создает ul
+// function createList() {
+//   const list = document.createElement('ul');
+//   list.classList.add('gallery');
+//   refs.container.insertAdjacentElement('beforeend', list);
+//   return;
+// }
+
+// //создает разметку галлереи
+// function createListItems(data) {
+//   const { hits } = data;
+//   const markupEl = markup(hits);
+
+//   const list = document.querySelector('.gallery');
+//   list.insertAdjacentHTML('beforeend', markupEl);
+// }
+
+// //создает кнопки ""загрузить больше" и "вверх""
+// function createBtn() {
+//   const loadMoreBtn = document.createElement('button');
+//   loadMoreBtn.textContent = 'Load more';
+//   loadMoreBtn.classList.add('loadMoreBtn');
+//   loadMoreBtn.classList.add('button');
+
+//   refs.container.insertAdjacentElement('beforeend', loadMoreBtn);
+//   loadMoreBtn.addEventListener('click', searchImages);
+
+//   const upBtn = document.createElement('a');
+//   upBtn.textContent = 'Up';
+//   upBtn.classList.add('upBtn');
+//   upBtn.href = '#submit';
+
+//   refs.container.insertAdjacentElement('beforeend', upBtn);
+// }
